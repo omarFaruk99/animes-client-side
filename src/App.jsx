18bd6f9c3ad7929f8587084d88917c0f95161201
omarFaruk <2035faruk@gmail.com>
2025-04-26 14:43:45 +0600

    initialize project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+    return (
+        <>
+            <div>
+                <h2 className={"font-bold bg-amber-300"}>Animes</h2>
+            </div>
+        </>
+    )
+}
+
+export default App
